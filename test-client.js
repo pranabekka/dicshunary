2f6e41e447cd175740781082964240c0ceff67e8
Pranab <pranabekka@gmail.com>
2025-07-03 19:43:02 +0530

    remove noisy logging functions

--- test-client.js
+++ test-client.js
@@ -6,25 +6,21 @@ const ws = new WebSocket(
 
 ws.onopen = (e) => {
 	console.log('SOCKET OPEN');
-	console.log(e);
 	ws.send(
 		JSON.stringify({event: 'join', name: 'nunya'})
 	);
 }
 
 ws.onmessage = (e) => {
-	console.log('SOCKET MESSAGE:');
-	console.log(e.data);
+	console.log('SOCKET MESSAGE');
 	const message = JSON.parse(e.data);
 	console.log(message);
 }
 
 ws.onerror = (e) => {
-	console.error('SOCKET ERROR:');
-	console.error(e);
+	console.error('SOCKET ERROR');
 }
 
 ws.onclose = (e) => {
-	console.log('SOCKET CLOSE:');
-	console.log(e);
+	console.log('SOCKET CLOSE');
 }
