6b825e54b6060a86fe91679acc75c98e31d7f738
Pranab <pranabekka@gmail.com>
2025-07-13 23:46:52 +0530

    save players in server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 
+// players :: { [playerID]: name }
+export const players = {};
+
 export function join(name, id) {
 	const newId = 'player-' + crypto.randomUUID();
+	players[newId] = name;
 	return { type: 'join-ack', name, id: newId };
 };
--- server_test.js
+++ server_test.js
@@ -3,37 +3,45 @@
 
 'use strict';
 
-import { join } from './server.js';
+import * as server from './server.js';
 import { assert } from 'jsr:@std/assert';
 
 Deno.test('get join acknowledgement', () => {
-	const { type } = join('Player');
-	assert(type === 'join-ack');
+	const { type } = server.join('Player');
+	assert(type === 'join-ack', 'should receive object with type "join-ack".');
 });
 
 Deno.test('confirm name on join', () => {
 	const name = 'Player';
-	const { name: messageName } = join('Player');
+	const { name: messageName } = server.join('Player');
 	assert(messageName === name, 'should return same name');
 });
 
 Deno.test('assign id on join', () => {
-	const { id: messageId } = join('IWannaID');
+	const { id: messageId } = server.join('IWannaID');
 	assert(
 		typeof messageId === 'string',
-		`expected "string", got "${typeof messageId}"`
+		`id should be "string". got "${typeof messageId}"`
 	);
 	assert(
 		messageId.startsWith('player-'),
-		`expected /player-.*/. got "${messageId}"`
+		`id should match /player-.*/. got "${messageId}"`
 	);
 	assert(
 		messageId.length === 43,
-		`expected id length 43, got length "${messageId.length}" for "${messageId}"`
+		`id length should be 43. got "${messageId.length}" for "${messageId}"`
 	);
 	assert(
 		messageId === 'player-54743dc5-e24d-4a41-bb15-9584c1226df6',
 		'id with `deno test --seed 1` should be "player-54743dc5-e24d-4a41-bb15-9584c1226df6"'
+	);
+});
+
+Deno.test('store players', () => {
+	const { id } = server.join('SaveMe');
+	assert(
+		Object.keys(server.players).includes(id),
+		`players object should have entry with id`
 	);
 });
 
