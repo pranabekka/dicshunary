e419c20d6fe4aeea28aecb00cda0ece30c258d40
Pranab <pranabekka@gmail.com>
2025-07-17 22:14:33 +0530

    add stage transition to guessing and fix min player count bug
    check for active players instead of total
    when checking minimum player count

--- everything.mjs
+++ everything.mjs
@@ -30,6 +30,13 @@ class Game {
 		this._updateGiver(player);
 	}
 
+	givingToGuessing(player, word) {
+		const progress = this._nextStage(player);
+		if (progress === true) {
+			this._word = word;
+		}
+	}
+
 	_updateGiver(player) {
 		if (this._giver === undefined || this._giver === null) {
 			this._giver = player;
@@ -44,14 +51,18 @@ class Game {
 		};
 	}
 
-	nextStage() {
-		if (this._players.size < 3) {
-			return;
+	_nextStage(player) {
+		const activePlayerCount = [...this._players.values()].filter(data => {
+			return data.status === 'active';
+		}).length;
+		if (player !== this._giver || activePlayerCount < 3) {
+			return false;
 		} else {
-			const idx = this._stages.indexOf(this._stage);
-			const next = idx === this._stages.length - 1 ?
-				0 : idx + 1;
-			this._stage = this._stages[next];
+			const idxCurr = this._stages.indexOf(this._stage);
+			const idxNext = idxCurr === this._stages.length - 1 ?
+				0 : idxCurr + 1;
+			this._stage = this._stages[idxNext];
+			return true;
 		}
 	}
 }
@@ -91,13 +102,27 @@ Deno.test('update player status on rejoin', () => {
 	);
 });
 
-Deno.test('enforce minimum player count for switching stage', () => {
+Deno.test('enforce minimum active player count for switching stage', () => {
 	const game = new Game();
-	const stage = game._stage;
-	game.nextStage();
+	const expected = game._stages[0];
+	game._nextStage();
 	assert(
-		game._stage === stage,
-		`game stage should be "${stage}" with less than three players. got "${game._stage}"`
+		game._stage === expected,
+		`game stage should be "${expected}" with 0 players. got "${game._stage}"`
+	);
+	const player1 = game.playerNew();
+	game._nextStage(player1);
+	assert(
+		game._stage === expected,
+		`game stage should be "${expected}" with 1 player (less than 3). got "${game._stage}"`
+	);
+	const player2 = game.playerNew();
+	const player3 = game.playerNew();
+	game.playerLeave(player3);
+	game._nextStage(player1);
+	assert(
+		game._stage === expected,
+		`game stage should be "${expected}" with less than three active players. got "${game._stage}"`
 	);
 });
 
@@ -146,10 +171,72 @@ Deno.test('set giver to next active player if current leaves', () => {
 	);
 });
 
+Deno.test('switch stage if giver issues command', () => {
+	const game = new Game();
+	const player1 = game.playerNew();
+	const player2 = game.playerNew();
+	const player3 = game.playerNew();
+	const player4 = game.playerNew();
+	game.givingToGuessing(player1, 'eecksampul');
+	const expected = game._stages[1];
+	assert(
+		game._stage === expected,
+		`game stage should be "${expected}". got "${game._stage}"`
+	);
+});
+
+Deno.test('do not switch stage if min players met but player not giver', () => {
+	const game = new Game();
+	const player1 = game.playerNew();
+	const player2 = game.playerNew();
+	const player3 = game.playerNew();
+	const player4 = game.playerNew();
+	game.givingToGuessing(player2, 'bad');
+	const expected = game._stages[0];
+	assert(
+		game._stage === expected,
+		`game stage should be "${expected}". got "${game._stage}"`
+	);
+});
+
+Deno.test('set word from giver in game', () => {
+	const game = new Game();
+	const player1 = game.playerNew();
+	const player2 = game.playerNew();
+	const player3 = game.playerNew();
+	const word = 'wurd';
+	game.givingToGuessing(player1, word);
+	assert(
+		game._word === word,
+		`word should be "${word}". got "${game._word}"`
+	);
+});
+
+Deno.test('do not set word if unable to progress stage', () => {
+	const game = new Game();
+	const player1 = game.playerNew();
+	const player2 = game.playerNew();
+	const player3 = game.playerNew();
+	const word = 'wurd';
+	game.givingToGuessing(player2, word);
+	const expected = undefined;
+	assert(
+		game._word === expected,
+		`word should be "${expected}". got "${game._word}"`
+	);
+	game.playerLeave(player3);
+	game.givingToGuessing(player1, word);
+	assert(
+		game._word === expected,
+		`word should be "${expected}". got "${game._word}"`
+	);
+});
+
 // Deno.test('-TEMPLATE-', () => {
 // 	const game = new Game();
 // 	const player = game.playerNew();
 // 	assert(
 // 		-ASSERTION-,
 // 		`-MESSAGE-`
+// 	);
 // });
